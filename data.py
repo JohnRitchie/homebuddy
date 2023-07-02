baa644594c138083be54d534db5efd1b9b1d5ae9
JohnRitchie <lasarenko91@mail.ru>
2023-07-02 18:38:28 +0300

    data

---
+++ data.py
@@ -0,0 +1,12 @@
+class Link:
+    siding_link = 'https://hb-eta.stage.sirenltd.dev/siding'
+    thank_you_link = 'https://hb-eta.stage.sirenltd.dev/thank-you'
+
+
+class AnswerData:
+    zip_code = '09090'
+    siding_area = '100'
+
+
+class ThankYouData:
+    contractor = 'QA Customer'
